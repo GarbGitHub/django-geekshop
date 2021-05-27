a3b5d2c4775c7f06cf323060171d3d0fd4238a55
Sergei_Klyuikov <garbich@gmail.com>
2021-05-27 10:50:26 +0300

    SECRET_KEY is called from the config.py

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+from .config import APP_SECRET_KEY
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#3b0rfj38u+v4p_)hl5f=o1g*m9m%x9rf6@6)%hem-f5bg+9f'
+SECRET_KEY = APP_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
