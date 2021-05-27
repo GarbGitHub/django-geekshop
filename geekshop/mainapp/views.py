15c4772b3b49cde4870a5d1c801ed829896508bc
Sergei_Klyuikov <garbich@gmail.com>
2021-05-27 17:37:05 +0300

    added to context: title, links_menu

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'каталог товаров'
+    links_menu = [
+        {'href': 'mainapp:products_all', 'name': 'все'},
+        {'href': 'mainapp:products_home', 'name': 'дом'},
+        {'href': 'mainapp:products_office', 'name': 'офис'},
+        {'href': 'mainapp:products_modern', 'name': 'модерн'},
+        {'href': 'mainapp:products_classic', 'name': 'классика'},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
