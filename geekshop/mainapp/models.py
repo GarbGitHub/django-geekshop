be69d3c9825fe144b1985bb24f59868ea47dd523
Sergei_Klyuikov <garbich@gmail.com>
2021-05-30 23:05:26 +0300

    added model Product

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -20,6 +20,52 @@ class ProductCategory(models.Model):
     # )
 
     def __str__(self):
-        return self.name
+        return f'{self.name} id: {self.id} -- {self.created}'
+
 
 # python manage.py makemigrations
+# python manage.py migrate
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        ProductCategory,
+        verbose_name='категория',
+        on_delete=models.CASCADE)
+
+    name = models.CharField(
+        verbose_name='название товара',
+        max_length=128
+    )
+
+    image = models.ImageField(  # pip install Pillow
+        verbose_name='изображение товара',
+        upload_to='products_images',
+        blank=True
+    )
+
+    short_desc = models.CharField(
+        verbose_name='краткое описание',
+        max_length=256,
+        blank=True
+    )
+
+    description = models.TextField(
+        verbose_name='описание товара',
+        blank=True
+    )
+
+    price = models.DecimalField(
+        verbose_name='цена товара',
+        max_digits=8,
+        decimal_places=2,
+        default=0
+    )
+
+    quantity = models.PositiveIntegerField(
+        verbose_name='количество на складе',
+        default=0
+    )
+
+
+
+
