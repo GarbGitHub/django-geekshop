b5614694bddf7458b944db5d2bb9fdea3cb0dd0f
Sergei_Klyuikov <garbich@gmail.com>
2021-06-01 14:06:17 +0300

    added dynamic address processing

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
+from .models import Product
 
 
-def products(request):
+def products(request, pk=None):
     title = 'каталог товаров'
+
     links_menu = [
-        {'href': 'mainapp:products_all', 'name': 'все'},
-        {'href': 'mainapp:products_home', 'name': 'дом'},
-        {'href': 'mainapp:products_office', 'name': 'офис'},
-        {'href': 'mainapp:products_modern', 'name': 'модерн'},
-        {'href': 'mainapp:products_classic', 'name': 'классика'},
+        {'href': 'products:products_all', 'name': 'все'},
+        {'href': 'products:products_home', 'name': 'дом'},
+        {'href': 'products:products_office', 'name': 'офис'},
+        {'href': 'products:products_modern', 'name': 'модерн'},
+        {'href': 'products:products_classic', 'name': 'классика'},
     ]
+
     context = {
         'title': title,
-        'links_menu': links_menu
+        'links_menu': links_menu,
     }
-    return render(request, 'mainapp/products.html', context=context)
+
+    template = 'mainapp/products.html'
+
+    if pk:
+        context['product'] = Product.objects.get(pk=pk)
+        template = 'mainapp/product-card.html'
+
+    return render(request, template, context=context)
