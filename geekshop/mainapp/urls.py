733600e55fade730175f0c0250d6b9aca86f22e8
Sergei_Klyuikov <garbich@gmail.com>
2021-06-01 14:06:40 +0300

    added dynamic address processing

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from . import views
+from .views import products
 
 app_name = 'mainapp'  # для работы 'namespace=' в geekshop/urls.py
 
 urlpatterns = [
-    path('', views.products, name='index'),
-    path('products_all/', views.products, name='products_all'),
-    path('products_home/', views.products, name='products_home'),
-    path('products_office/', views.products, name='products_office'),
-    path('products_modern/', views.products, name='products_modern'),
-    path('products_classic/', views.products, name='products_classic'),
+    path('', products, name='index'),
+    path('product/<int:pk>', products, name='product'),
+    path('products_all/', products, name='products_all'),
+    path('products_home/', products, name='products_home'),
+    path('products_office/', products, name='products_office'),
+    path('products_modern/', products, name='products_modern'),
+    path('products_classic/', products, name='products_classic'),
 ]
