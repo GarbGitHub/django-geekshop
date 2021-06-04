ee7158dd8a4d4942bb3d5bbfd39eddbcf4436036
Sergei_Klyuikov <garbich@gmail.com>
2021-06-04 09:58:16 +0300

    created controllers login, logout

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from authapp.forms import ShopUserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
-# Create your views here.
+
+def login(request):
+    title = 'вход'
+
+    login_form = ShopUserLoginForm(data=request.POST)  # Все данные из формы полученные методом POST
+    if request.method == 'POST' and login_form.is_valid():  # если POST
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)  # авторизован или нет
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+    content = {'title': title,
+               'login_form': login_form,}
+    return render(request, 'authapp/login.html', content)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
