b01a6a379ffe6d1e886546b573e2cc1f7b21bbcf
Sergei_Klyuikov <garbich@gmail.com>
2021-09-04 21:06:59 +0300

    edit settings.py

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -116,13 +116,6 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -161,11 +154,27 @@ STATIC_URL = '/static/'  # Путь к статике из браузера
 
 if DEBUG:  # Локальный сервер
 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'geekshop', 'static'),  # Путь к статике на сервере
     )
 
 else:  # Боевой сервер
+
+    DATABASES = {
+        'default': {
+            'NAME': 'geekshop-1',
+            'ENGINE': 'django.db.backends.postgresql',
+            'USER': 'postgres',
+        }
+    }
+
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
     STATICFILES_FINDERS = [
